ff0bc9db35018f6d90f529a41e11c1e38bcf9150
hugo <hugo@gensdeconfiance.com>
2021-12-19 15:49:25 +0100

    better lexer

--- src/main.zig
+++ src/main.zig
@@ -1,47 +1,116 @@
 const std = @import("std");
 
-fn parseLines(buffer: []const u8) ![]const u8 {
-    var line_it = std.mem.tokenize(buffer, "\n");
-    var result = std.ArrayList(u8).init(std.heap.page_allocator);
-    while (line_it.next()) |line| {
-        if (line[0] == '-') {
-            try result.appendSlice("\n--");
-        }
-        try result.appendSlice(line);
-    }
-    return result.items;
-}
+const Token = struct {
+    tag: Tag,
+    loc: Loc,
 
-fn parseDialog(buffer: []const u8) ![]const u8 {
-    std.debug.assert(buffer.len > 0 and buffer[0] == '"');
-    var endOfDialogIndex = std.mem.indexOfScalar(u8, buffer[1..], '"').?;
-    var result = std.ArrayList(u8).init(std.heap.page_allocator);
-    {
-        var buf: [5]u8 = undefined;
-        var size = try std.unicode.utf8Encode('«', &buf);
-        for (buf[0..size]) |c| {
-            try result.append(c);
-        }
-    }
-    const remaining = buffer[1..(endOfDialogIndex + 1)];
-    const parsedLines = try parseLines(remaining);
-    try result.appendSlice(parsedLines);
-    {
-        var buf: [5]u8 = undefined;
-        var size = try std.unicode.utf8Encode('»', &buf);
-        for (buf[0..size]) |c| {
-            try result.append(c);
-        }
+    const Loc = struct {
+        start: usize,
+        end: usize,
+    };
+
+    const Tag = enum {
+        hashtag,
+        dashDialogStart,
+        text,
+        quotationMark,
+        underscore,
+        eof,
+    };
+};
+
+const Tokenizer = struct {
+    buffer: [:0]const u8,
+    index: usize,
+
+    pub fn init(buffer: [:0]const u8) Tokenizer {
+        return .{
+            .buffer = buffer,
+            .index = 0,
+        };
     }
 
-    return result.items;
-}
+    const State = enum {
+        start,
+        newline,
+        text,
+    };
+
+    pub fn next(self: *Tokenizer) Token {
+        var state: State = .start;
+        var result = Token{
+            .tag = .eof,
+            .loc = .{
+                .start = self.index,
+                .end = undefined,
+            },
+        };
+
+        while (true) : (self.index += 1) {
+            const c = self.buffer[self.index];
+            switch (state) {
+                .start => switch (c) {
+                    0 => break,
+                    '\n' => {
+                        state = .newline;
+                    },
+                    '#' => {
+                        result.tag = .hashtag;
+                        self.index += 1;
+                        break;
+                    },
+                    '_' => {
+                        result.tag = .underscore;
+                        self.index += 1;
+                        break;
+                    },
+                    '"' => {
+                        result.tag = .quotationMark;
+                        self.index += 1;
+                        break;
+                    },
+                    else => {
+                        result.tag = .text;
+                        state = .text;
+                    },
+                },
+                .newline => switch (c) {
+                    '-' => {
+                        result.tag = .dashDialogStart;
+                        self.index += 1;
+                        break;
+                    },
+                    else => {
+                        state = .start;
+                    },
+                },
+                .text => switch (c) {
+                    '\n', '_' => {
+                        //self.index -= 1;
+                        state = .start;
+                        break;
+                    },
+                    else => {},
+                },
+            }
+        }
+
+        if (result.tag == .eof) {
+            result.loc.start = self.index;
+        }
+        result.loc.end = self.index;
+        return result;
+    }
+};
 
 pub fn main() anyerror!void {
-    std.log.info("{s}", .{try parseDialog("\"bonjour\"")});
+    const input = "# Le titre du chapitre\n\n\"bonjour\n- ça va ?\n- oui et toi _Jean-Jacques_ ?\"\n\"\"";
+    var tokenizer = Tokenizer.init(input);
+
+    var token = tokenizer.next();
+    while (token.tag != .eof) : (token = tokenizer.next()) {
+        std.log.info("{}", .{token});
+    }
 }
 
-test "parseDialog" {
-    try std.testing.expect(std.mem.eql(u8, try parseDialog("\"bonjour\""), "«bonjour»"));
-    try std.testing.expect(std.mem.eql(u8, try parseDialog("\"bonjour\n- ça va ?\n- oui et toi Jean-Jacques ?\""), "«bonjour\n--- ça va ?\n--- oui et toi Jean-Jacques ?»"));
-}
+test "parseDialog" {}
